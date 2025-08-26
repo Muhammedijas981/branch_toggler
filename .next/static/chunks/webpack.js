14469e23c03800e5aabcded231e35b05a5243d7e
Muhammedijas981 <muhammedijas981@gmail.com>
2025-08-26 13:05:09 +0530

    fix added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd8afd86871af6a1"; }
+/******/ 		__webpack_require__.h = function() { return "278dd3ba80fc5473"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
